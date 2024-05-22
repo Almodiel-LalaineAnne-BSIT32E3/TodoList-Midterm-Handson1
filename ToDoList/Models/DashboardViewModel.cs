eb0aee261ebf9fd3ce0352f7bb90d042945b779c
Aika <Aika@LAPTOP-LUN36AVI>
2024-05-22 19:55:42 +0800

    Add project files.

---
+++ ToDoList/Models/DashboardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using ToDoList.Domain;
+
+namespace ToDoList.Models
+{
+    public class DashboardViewModel
+    {
+       
+        public List<ToDoItem> ToDoItems { get; set; }
+
+        public List<ToDoItem> PendingToDoItems { get; set; }
+
+        public List<ToDoItem> DoneToDoItems { get; set; }
+
+        public string Username { get; set; }  
+        public int PendingCount { get; set; } 
+        public int DoneCount { get; set; }    
+
+        
+        public DashboardViewModel()
+        {
+            ToDoItems = new List<ToDoItem>();
+            PendingToDoItems = new List<ToDoItem>();
+            DoneToDoItems = new List<ToDoItem>();
+        }
+    }
+}
